2f9b13bd4b4761a1c9602e8f183ffd982054e33a
Bartek Jasek <BartekJasek4@gmail.com>
2023-07-08 17:02:18 +0200

    add results of the game

--- js/Script.js
+++ js/Script.js
@@ -28,3 +28,22 @@ else if  (playerInput == '3') {
 
 printMessage('Twój ruch to: ' + playerMove);
 
+
+
+if (computerMove === 'kamień' && playerMove === 'papier') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove === 'kamień' && playerMove === 'nożyce') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove === 'papier' && playerMove === 'kamień') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove === 'papier' && playerMove === 'nożyce') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove === 'nożyce' && playerMove === 'kamień') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove === 'nożyce' && playerMove === 'papier') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove === playerMove) {
+  printMessage('Remis!'); }
+ else {
+  printMessage('Nieznany ruch. Spróbuj jeszcze raz.');
+}
